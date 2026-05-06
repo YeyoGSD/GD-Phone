dc07a74214b41f053ebe276360cc0d747e8f1edf
TakisAlPastor <alfredoccm@protonmail.com>
2026-05-06 12:45:50 -0700

    Code style and cleaning refactor

--- Apps/1_Chat/ChatDetail/chat_detail.gd
+++ Apps/1_Chat/ChatDetail/chat_detail.gd
@@ -1,8 +1,7 @@
 class_name ChatDetail
 extends Control
 
-const BUBBLE_ME = preload("uid://cnmccr7wi7o5r")
-const BUBBLE_OTHER = preload("uid://dcqg1uuswi4y8")
+signal requested_go_back
 
 @onready var messages_container: VBoxContainer = %MessagesContainer
 @onready var name_label: Label = %NameLabel
@@ -14,69 +13,67 @@ const BUBBLE_OTHER = preload("uid://dcqg1uuswi4y8")
 @onready var keyboard_input_container: HBoxContainer = %KeyboardInputContainer
 @onready var choice_input_container: HBoxContainer = %ChoiceInputContainer
 
-signal requested_go_back
-
-func _ready() -> void:
-	back_button.pressed.connect(func() -> void:
-		requested_go_back.emit())
-
-	type_box.text_submitted.connect(func(_text: String) -> void:
-		_on_send_pressed())
-
-	send_button.pressed.connect(_on_send_pressed)
+const BUBBLE_ME = preload("uid://cnmccr7wi7o5r")
+const BUBBLE_OTHER = preload("uid://dcqg1uuswi4y8")
 
 
 func setup(chat_data: ChatData) -> void:
 	name_label.text = chat_data.contact.name
+	
 	if chat_data.contact.avatar:
 		avatar.texture = chat_data.contact.avatar
-
+	
 	for child in messages_container.get_children():
 		child.queue_free()
-
+	
 	for msg in chat_data.conversation:
-		add_message_bubble(msg)
+		_add_message_bubble(msg)
+	
+	_scroll_to_bottom()
 
-	scroll_to_bottom()
+func _ready() -> void:
+	back_button.pressed.connect(_on_back_button_pressed)
+	type_box.text_submitted.connect(_on_text_submitted)
+	send_button.pressed.connect(_on_send_pressed)
 
 
-func add_message_bubble(msg_data: MessageData) -> void:
+func _add_message_bubble(msg_data: MessageData) -> void:
 	var bubble: ChatBubble
-
+	
 	if msg_data.sender == MessageData.Sender.ME:
 		bubble = BUBBLE_ME.instantiate() as ChatBubble
 	else:
 		bubble = BUBBLE_OTHER.instantiate() as ChatBubble
-
+	
 	messages_container.add_child(bubble)
 	bubble.setup(msg_data.text, msg_data.audio, msg_data.image, msg_data.link)
-
+	
 	if not msg_data.reply_options.is_empty():
-		show_options(msg_data.reply_options)
-
+		_show_options(msg_data.reply_options)
+	
 	if msg_data.on_read_event:
 		EventManager.execute(msg_data.on_read_event)
 
 
-func trigger_npc_reply(message: MessageData) -> void:
+func _trigger_npc_reply(message: MessageData) -> void:
 	# TODO: Agregar animación de escribiendo
 	await get_tree().create_timer(message.delay).timeout
-	add_message_bubble(message)
-	scroll_to_bottom()
-
+	_add_message_bubble(message)
+	_scroll_to_bottom()
+	
 	if message.next_message_auto:
-		trigger_npc_reply(message.next_message_auto)
+		_trigger_npc_reply(message.next_message_auto)
 	elif not message.reply_options.is_empty():
-		show_options(message.reply_options)
+		_show_options(message.reply_options)
 
 
-func show_options(options: Array[ReplyOption]) -> void:
+func _show_options(options: Array[ReplyOption]) -> void:
 	keyboard_input_container.hide()
 	choice_input_container.show()
-
+	
 	for child in choice_input_container.get_children():
 		child.queue_free()
-
+	
 	for option in options:
 		var btn := Button.new()
 		btn.custom_minimum_size.x = 150
@@ -85,13 +82,13 @@ func show_options(options: Array[ReplyOption]) -> void:
 		choice_input_container.add_child(btn)
 
 
-func scroll_to_bottom() -> void:
-	# Espera un frame para que Godot recalcule el tamaño de la nueva burbuja
+func _scroll_to_bottom() -> void:
+	# Wait one frame to let Godot recalculate the size of the new bubble
 	await get_tree().process_frame
 	message_scroll.set_deferred(&"scroll_vertical", message_scroll.get_v_scroll_bar().max_value )
 
 
-func create_message(text: String, sender: MessageData.Sender) -> MessageData:
+func _create_message(text: String, sender: MessageData.Sender) -> MessageData:
 	var msg := MessageData.new()
 	msg.text = text
 	msg.sender = sender
@@ -102,26 +99,33 @@ func _on_send_pressed() -> void:
 	var text := type_box.text.strip_edges()
 	if text.is_empty():
 		return
-
-	var new_msg := create_message(text, MessageData.Sender.ME)
-	add_message_bubble(new_msg)
-
+	
+	var new_msg := _create_message(text, MessageData.Sender.ME)
+	_add_message_bubble(new_msg)
+	
 	type_box.text = ""
-
-	scroll_to_bottom()
-
+	
+	_scroll_to_bottom()
 
 
 func _on_option_selected(option: ReplyOption) -> void:
-	var msg := create_message(option.text, MessageData.Sender.ME)
-
-	add_message_bubble(msg)
-	scroll_to_bottom()
-
+	var msg := _create_message(option.text, MessageData.Sender.ME)
+	
+	_add_message_bubble(msg)
+	_scroll_to_bottom()
+	
 	choice_input_container.hide()
 	keyboard_input_container.show()
-
+	
 	if option.target_message:
-		trigger_npc_reply(option.target_message)
+		_trigger_npc_reply(option.target_message)
 	else:
 		print("Fin de la conversación (No hay target_message)")
+
+
+func _on_back_button_pressed() -> void:
+	requested_go_back.emit()
+
+
+func _on_text_submitted() -> void:
+	_on_send_pressed()
